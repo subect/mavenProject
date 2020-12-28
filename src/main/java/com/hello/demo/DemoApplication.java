28bbe9a74c7e83b6e90dffcc49a96ebfe7f0c35a
hhxx66 <1901608396@qq.com>
2020-12-28 21:35:58 +0800

    maven hello

---
+++ src/main/java/com/hello/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.hello.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "controller")
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
